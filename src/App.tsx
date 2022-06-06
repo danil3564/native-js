2a7cce70f18c6f9c3b3c0ff547a448f0995b6376
Daniil <dfefelov@bk.ru>
2022-06-07 00:40:56 +0700

    lesson 3 didn't finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import {Functions} from "./03/Functions";
 
 function App() {
   return (
     <div className="App">
-      <Functions />
     </div>
   );
 }
